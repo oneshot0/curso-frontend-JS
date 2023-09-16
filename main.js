a494752027c471d8ebb031dbec0abb0c2caaea5c
jefry_nm <jefroy95@gmail.com>
2023-09-16 03:50:13 -0500

    Unión del Carrito de compras y uso de 2 métodos en JS para ocultar pantallas

--- main.js
+++ main.js
@@ -6,13 +6,36 @@ const menuEmail = $('.navbar-email');
 const desktopMenu = $('.desktop-menu');
 const menuHamIcon = $('.menu');
 const mobileMenu = $('.mobile-menu');
+const menuCarritoIcon = $('.navbar-shopping-cart');
+const aside = $('.product-detail');
 
 
-menuEmail.addEventListener('click', () => desktopMenu.classList.toggle('inactive'));
+menuEmail.addEventListener('click', () => desktopMenu.classList.toggle('inactive') + aside.classList.add('inactive'));
 menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 
 function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    aside.classList.add('inactive');
+  }
+
   mobileMenu.classList.toggle('oculto');
 }
+
+function toggleCarritoAside() {
+  //1er método para ocultar 
+  const isMobileMenuClosed = mobileMenu.classList.contains('oculto');
+  
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add('oculto');
+  }
+  //2do método para ocultar 
+    aside.classList.toggle('inactive');
+    desktopMenu.classList.add('inactive');
+
+}
+
 
